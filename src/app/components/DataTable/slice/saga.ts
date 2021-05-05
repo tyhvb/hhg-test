9aef8b14330e658802b7d349611d9f0c747adc4f
tyhvb <tyhvb@fabatechnology.com>
2021-05-05 09:25:42 +0700

    fix: update api url config

--- src/app/components/DataTable/slice/saga.ts
+++ src/app/components/DataTable/slice/saga.ts
@@ -1,11 +1,12 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import { dataTableActions as actions } from '.';
 import axios from 'axios';
-import { API_URL } from 'constant/urlConstant';
 
 function* getData() {
   try {
-    const data = yield axios.get(API_URL).then(response => response.data);
+    const data = yield axios
+      .get(`${process.env.REACT_APP_BASE_API_URL}`)
+      .then(response => response.data);
 
     yield put(actions.successGetData(data));
   } catch (errors) {
@@ -17,7 +18,7 @@ function* updateEmployee(action) {
   const { name, email, position } = action.payload;
   try {
     yield axios
-      .post(API_URL, {
+      .post(`${process.env.REACT_APP_BASE_API_URL}`, {
         name,
         email,
         position,
